c61b651e16ed8aba7a3e174f513156835f3807e3
Anton Kobyshev <antonkobyshev@gmail.com>
2023-08-19 18:06:46 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./modules/Header/Header";
+import ModalNotice from "./modules/ModalNotice/ModalNotice";
+import ModalCongrats from "./modules/ModalCongrats/ModalCongrats";
+import ModalDelete from "./modules/ModalDelete/ModalDelete";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ModalNotice />
+      <ModalCongrats />
+      <ModalDelete />
+    </div>
+  );
+}
+
+export default App;
