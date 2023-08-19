0cf363aabfed89d9e62f8cff88518ac3f707085c
Anton Kobyshev <antonkobyshev@gmail.com>
2023-08-19 17:30:09 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <header className="App-header"></header>
-
       <ModalNotice />
       <ModalCongrats />
       <ModalDelete />
