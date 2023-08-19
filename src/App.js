9fb1228cf414bae5ad7bcae923abba5670cfd9c5
Anton Kobyshev <antonkobyshev@gmail.com>
2023-08-19 14:22:57 +0300

    updt

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
 import ModalNotice from "./modules/ModalNotice/ModalNotice";
+import ModalCongrats from "./modules/ModalCongrats/ModalCongrats";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <ModalNotice />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Here would be super App!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ModalCongrats />
       </header>
     </div>
   );
