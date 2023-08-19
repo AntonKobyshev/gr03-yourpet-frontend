dd9edccdd44128c31a9ebe6403896ba767da8357
TarasVarvarych <varvarychtaras@gmail.com>
2023-08-19 23:02:26 +0300

    add main page

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,9 @@
+import css from "./MainPage.module.css";
+
+export default function MainPage() {
+  return (
+    <div className={css.background}>
+      <h1 className={css.title}>Take good care of your small pets</h1>
+    </div>
+  );
+}
