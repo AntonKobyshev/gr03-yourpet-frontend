f7255bf8c30f3d146d7f1e28769553040fbf496a
Sergey Shmatok <trycifer@gmail.com>
2023-08-21 00:22:31 +0300

    Update Private.jsx

--- src/Routes/Private/Private.jsx
+++ src/Routes/Private/Private.jsx
@@ -1,1 +1,21 @@
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
 
+import { selectAuth } from "../../redux/auth/auth-selectors";
+import Loader from "../../shared/components/Loader/Loader";
+
+const PrivateRoute = () => {
+  const { isLoggedIn, token } = useSelector(selectAuth);
+
+  if (!isLoggedIn && token) {
+    return <Loader />;
+  }
+
+  if (!isLoggedIn && !token) {
+    return <Navigate to="/login" />;
+  }
+
+  return <Outlet />;
+};
+
+export default PrivateRoute;
