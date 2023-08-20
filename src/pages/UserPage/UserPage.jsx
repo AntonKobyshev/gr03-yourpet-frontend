abbaf051202b9912bca4a27c2e69c5e5eab50bee
Anna Shalar <113612033+latikaann@users.noreply.github.com>
2023-08-20 16:52:53 +0300

    додала розмітку сторінки Profile

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import css from "./UserPage.module.css";
 import UserData from "../../modules/User/UserData/UserData";
+import PetsData from "../../modules/User/PetsData/PetsData";
 
 const UserPage = () => {
   return (
-    <>
-      <div className={css.userInfoContainer}>
+    <div className={css.container}>
+      <div className={css.userContainer}>
         <p className={css.title}>My information:</p>
         <UserData></UserData>
       </div>
       <div className={css.petsContainer}>
         <p className={css.title}>My pets:</p>
+        <PetsData />
       </div>
-    </>
+    </div>
   );
 };
 
