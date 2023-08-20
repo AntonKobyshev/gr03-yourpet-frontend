02449497b6944fe06a7658155e806036be998523
Markiyan333 <markiyan333@gmail.com>
2023-08-20 17:56:38 +0300

    add two forms

---
+++ src/pages/AddPetPage/formPersonalDetails/PersonalDetails.jsx
@@ -0,0 +1,54 @@
+import { Label, Input, PersonalDetailsWrapper } from './PersonalDetails.styled';
+
+const PersonalDetails = ({ category, touched, errors }) => {
+  return (
+    <PersonalDetailsWrapper category={category}>
+      {category !== 'my-pet' && (
+        <Label>
+          Title of add
+          <Input
+            type="text"
+            name="title"
+            placeholder="Title of add"
+            errors={touched.title && errors.title}
+          />
+          {touched.title && errors.title && <div>{errors.title}</div>}
+        </Label>
+      )}
+      <Label>
+        Name pet
+        <Input
+          type="text"
+          name="name"
+          placeholder="Type name pet"
+          errors={touched.name && errors.name}
+        />
+        {touched.name && errors.name && <div>{errors.name}</div>}
+      </Label>
+      <Label>
+        Date of birth
+        <Input
+          type="text"
+          name="dateOfBirth"
+          placeholder="dd.mm.yyyy"
+          errors={touched.dateOfBirth && errors.dateOfBirth}
+        />
+        {touched.dateOfBirth && errors.dateOfBirth && (
+          <div>{errors.dateOfBirth}</div>
+        )}
+      </Label>
+      <Label>
+        Breed
+        <Input
+          type="text"
+          name="breed"
+          placeholder="Type breed"
+          errors={touched.breed && errors.breed}
+        />
+        {touched.breed && errors.breed && <div>{errors.breed}</div>}
+      </Label>
+    </PersonalDetailsWrapper>
+  );
+};
+
+export default PersonalDetails;
---
+++ src/pages/AddPetPage/formPersonalDetails/PersonalDetails.styled.jsx
@@ -0,0 +1,43 @@
+import styled from '@emotion/styled';
+import { Field, ErrorMessage } from 'formik';
+
+export const PersonalDetailsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+/
+`;
+
+export const Label = styled.label`
+  position: relative;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: calc(19 / 14);
+  color: #111111;
+
+
+`;
+
+export const Input = styled(Field)`
+  width: 100%;
+  padding: 9px 15px;
+  margin-top: 4px;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 1.5;
+
+  outline: none;
+  border-radius: 40px;
+  border: 1px solid;
+
+`;
+
+export const Message = styled(ErrorMessage)`
+  position: absolute;
+  bottom: -15px;
+  left: 20px;
+  font-size: 12px;
+  font-weight: 400;
+  line-height: calc(16 / 12);
+  color: #f43f5e;
+`;
