a130c71a7ea7c91c27b45eac0988b50432cae97a
Sergey Shmatok <trycifer@gmail.com>
2023-08-20 19:48:36 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { lazy } from "react";
 import SharedLayout from "./shared/components/SharedLayout/SharedLayout";
-import NoticesCategoriesList from "./modules/Notices/NoticesCategoriesList/NoticesCategoriesList";
+// import NoticesCategoriesList from "./modules/Notices/NoticesCategoriesList/NoticesCategoriesList";
 
 const NoticesPage = lazy(() => import("./pages/NoticesPage/NoticesPage"));
 
@@ -13,12 +13,12 @@ function App() {
         <Route path="/register" element={null} />
         <Route path="/login" element={null} />
         <Route path="/notices" element={<NoticesPage />}>
-          <Route index element={<NoticesCategoriesList />} />
+          {/* <Route index element={<NoticesCategoriesList />} />
           <Route path="sell" element={<NoticesCategoriesList />} />
           <Route path="lost-found" element={<NoticesCategoriesList />} />
           <Route path="for-free" element={<NoticesCategoriesList />} />
           <Route path="favorite" element={<NoticesCategoriesList />} />
-          <Route path="own" element={<NoticesCategoriesList />} />
+          <Route path="own" element={<NoticesCategoriesList />} /> */}
         </Route>
         <Route path="/news" element={null} />
         <Route path="/friends" element={null} />
