4056e662d6650caa61375df4a920570d2643e938
Anna Shalar <113612033+latikaann@users.noreply.github.com>
2023-08-21 14:04:13 +0300

    add confirm

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
-
 import { Routes, Route, Navigate } from "react-router-dom";
 import { lazy } from "react";
 import SharedLayout from "./shared/components/SharedLayout/SharedLayout";
 import MainPage from "./pages/MainPage/MainPage";
 import NoticesCategoriesList from "./modules/Notices/NoticesCategoriesList";
 import NoticesPage from "./pages/NoticesPage/NoticesPage";
+import UserPage from "./pages/UserPage/UserPage";
 
-const RegisterPage = lazy(() => import('./pages/RegisterPage/RegisterPage'));
-const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'))
-
+const RegisterPage = lazy(() => import("./pages/RegisterPage/RegisterPage"));
+const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route path="/main" element={<MainPage />} index />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/notices" element={<NoticesPage />}>
-          <Route index element={<Navigate to="sell" replace />} />
-          <Route path=":categoryName" element={<NoticesCategoriesList />} />
+    <>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route path="/main" element={<MainPage />} index />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/notices" element={<NoticesPage />}>
+            <Route index element={<Navigate to="sell" replace />} />
+            <Route path=":categoryName" element={<NoticesCategoriesList />} />
+          </Route>
+          <Route path="/news" element={null} />
+          <Route path="/friends" element={null} />
         </Route>
-        <Route path="/news" element={null} />
-        <Route path="/friends" element={null} />
-      </Route>
-    </Routes>
+      </Routes>
+
+      <UserPage />
+    </>
   );
 }
 
--- src/modules/User/UserForm/UserForm.jsx
+++ src/modules/User/UserForm/UserForm.jsx
@@ -2,12 +2,50 @@ import React, { useState } from "react";
 import { Formik, Form, Field } from "formik";
 import css from "./UserForm.module.css";
 import Logout from "../UserForm/Logout";
+import CameraIcon from "../../../images/icons/camera.svg";
+import CheckIcon from "../../../images/icons/check.svg";
+import CrossIcon from "../../../images/icons/cross14.svg";
 
 const UserForm = ({ initialValues, editing, onEdit }) => {
   const [avatarPreview, setAvatarPreview] = useState(null);
+  const [avatarUploaded, setAvatarUploaded] = useState(false);
+  const [showConfirmButtons, setShowConfirmButtons] = useState(false);
 
   const handleSaveClick = () => {
     onEdit(false);
+  };
+
+  const handleEditPhotoClick = () => {
+    const fileInput = document.getElementById("avatar");
+
+    fileInput.click();
+  };
+
+  const handleAvatarChange = (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setAvatarPreview(reader.result);
+        setAvatarUploaded(true);
+        setShowConfirmButtons(true);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setAvatarPreview(null);
+      setAvatarUploaded(false);
+      setShowConfirmButtons(false);
+    }
+  };
+
+  const handleDeleteAvatar = () => {
+    setAvatarPreview(null);
+    setShowConfirmButtons(false);
+  };
+
+  const handleConfirmClick = () => {
+    setShowConfirmButtons(false);
   };
 
   return (
@@ -16,60 +54,105 @@ const UserForm = ({ initialValues, editing, onEdit }) => {
         {avatarPreview && (
           <img src={avatarPreview} alt="avatar" className={css.userImage} />
         )}
+
+        {editing && !showConfirmButtons && (
+          <button
+            type="button"
+            className={css.editPhotoButton}
+            onClick={handleEditPhotoClick}
+          >
+            <img src={CameraIcon} alt={"Camera icon"} />
+            Edit photo
+          </button>
+        )}
+
+        {editing && avatarUploaded && showConfirmButtons && (
+          <div className={css.confirmButtons}>
+            <button
+              type="button"
+              className={css.confirmBtn}
+              onClick={handleConfirmClick}
+            >
+              <img src={CheckIcon} alt={"Check icon"} />
+            </button>
+            <p>Confirm</p>
+            <button
+              type="button"
+              className={css.deleteBtn}
+              onClick={handleDeleteAvatar}
+            >
+              <img src={CrossIcon} alt={"Cross icon"} />
+            </button>
+          </div>
+        )}
       </div>
 
       <div>
         <Formik initialValues={initialValues}>
-          <Form className={css.form}>
-            <div className={css.formLabelBox}>
-              <label htmlFor="name">Name:</label>
+          <Form>
+            <div className={css.formAvatar}>
               <Field
                 className={css.input}
-                type="text"
-                id="name"
-                name="name"
+                type="file"
+                id="avatar"
+                name="avatar"
+                accept=".jpg, .jpeg, .png"
+                onChange={handleAvatarChange}
+                style={{ display: "none" }}
                 readOnly={!editing}
               />
             </div>
-            <div className={css.formLabelBox}>
-              <label htmlFor="email">Email:</label>
-              <Field
-                className={css.input}
-                type="number"
-                id="email"
-                name="email"
-                readOnly={!editing}
-              />
-            </div>
-            <div className={css.formLabelBox}>
-              <label htmlFor="birthday">Birthday:</label>
-              <Field
-                className={css.input}
-                type="text"
-                id="birthday"
-                name="birthday"
-                readOnly={!editing}
-              />
-            </div>
-            <div className={css.formLabelBox}>
-              <label htmlFor="phone">Phone:</label>
-              <Field
-                className={css.input}
-                type="number"
-                id="phone"
-                name="phone"
-                readOnly={!editing}
-              />
-            </div>
-            <div className={css.formLabelBox}>
-              <label htmlFor="city">City:</label>
-              <Field
-                className={css.input}
-                type="text"
-                id="city"
-                name="city"
-                readOnly={!editing}
-              />
+            <div className={css.inputsContainer}>
+              <div className={css.formLabelBox}>
+                <label htmlFor="name">Name:</label>
+                <Field
+                  className={css.input}
+                  type="text"
+                  id="name"
+                  name="name"
+                  readOnly={!editing}
+                />
+              </div>
+              <div className={css.formLabelBox}>
+                <label htmlFor="email">Email:</label>
+                <Field
+                  className={css.input}
+                  type="number"
+                  id="email"
+                  name="email"
+                  readOnly={!editing}
+                />
+              </div>
+              <div className={css.formLabelBox}>
+                <label htmlFor="birthday">Birthday:</label>
+                <Field
+                  className={css.input}
+                  type="text"
+                  id="birthday"
+                  name="birthday"
+                  readOnly={!editing}
+                />
+              </div>
+              <div className={css.formLabelBox}>
+                <label htmlFor="phone">Phone:</label>
+                <Field
+                  className={css.input}
+                  type="number"
+                  id="phone"
+                  name="phone"
+                  readOnly={!editing}
+                />
+              </div>
+              <div className={css.formLabelBox}>
+                <label htmlFor="city">City:</label>
+                <Field
+                  className={css.input}
+                  type="text"
+                  id="city"
+                  name="city"
+                  readOnly={!editing}
+                />
+              </div>
             </div>
           </Form>
         </Formik>
