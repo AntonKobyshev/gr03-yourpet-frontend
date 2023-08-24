3f7de280f7967163127886b447ef6280134bc798
Anton Kobyshev <antonkobyshev@gmail.com>
2023-08-24 13:03:02 +0300

    Update Header.jsx

--- src/modules/Header/Header.jsx
+++ src/modules/Header/Header.jsx
@@ -13,7 +13,9 @@ export default function Header() {
       <div className={css.shouldShowNav}>
         <Nav />
       </div>
-      {isLoggedIn ? <UserNav /> : <AuthNav />}
+      {/* {isLoggedIn ? <UserNav /> : <AuthNav />} */}
+      <UserNav />
+      <AuthNav />
     </header>
   );
 }
