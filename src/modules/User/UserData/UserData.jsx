504db5f5b0ccf9475b4778e1f1c36d2719614040
Anton Kobyshev <antonkobyshev@gmail.com>
2023-08-31 11:35:13 +0300

    fix user

--- src/modules/User/UserData/UserData.jsx
+++ src/modules/User/UserData/UserData.jsx
@@ -6,8 +6,6 @@ import UserForm from "../UserForm/UserForm";
 import { selectIsLoading } from "../../../redux/auth/auth-selectors";
 import { fetchUser } from "../../../redux/auth/auth-operations";
 
-import css from "./UserData.module.css";
-
 const UserCard = () => {
   const isLoading = useSelector(selectIsLoading);
   const dispatch = useDispatch();
